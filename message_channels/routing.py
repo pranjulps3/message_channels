e333b62f68a8ab79fcad3b9302beee2178ac1feb
Pranjul Shukla <pranjul.ps3@gmail.com>
2017-10-19 23:45:02 +0530

    Chat routing enhanced

--- message_channels/routing.py
+++ message_channels/routing.py
@@ -3,7 +3,7 @@ from chat_messages.consumers import message, add, disconnect
 
 channel_routing = [
     # route("http.request", first_channel),
-    route("websocket.connect", add),    				#path="r^/(?P<room_name>[a-zA-Z0-9_]+)/$"),
-    route("websocket.disconnect", disconnect), 		#path="r^/(?P<room_name>[a-zA-Z0-9_]+)/$"),
-    route("websocket.receive", message), 			#path="r^/(?P<room_name>[a-zA-Z0-9_]+)/$"),
+    route("websocket.connect", add, path=r"/chat/"),    				#path="r^/(?P<room_name>[a-zA-Z0-9_]+)/$"),
+    route("websocket.disconnect", disconnect, path=r"/chat/"), 		#path="r^/(?P<room_name>[a-zA-Z0-9_]+)/$"),
+    route("websocket.receive", message, path=r"/chat/"), 			#path="r^/(?P<room_name>[a-zA-Z0-9_]+)/$"),
 ]
