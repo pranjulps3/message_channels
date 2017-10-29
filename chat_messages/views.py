0b2899d09a2b38f34b9d1aebfb83d0759d8d8f49
Pranjul Shukla <pranjul.ps3@gmail.com>
2017-10-29 23:09:53 +0530

    added functionality

--- chat_messages/views.py
+++ chat_messages/views.py
@@ -19,11 +19,12 @@ def base(request, id):
 	tomessages = Message.objects.filter(sender=recipient, recipient=request.user)
 	messages = tomessages.union(mymessages)
 	last_id = -1
-	if tomessages.count()>=50:
-		messages = messages.order_by('timestamp')[:50]
+	cnt = messages.count()
+	if cnt>=50:
+		messages = messages.order_by('timestamp')[cnt-50:cnt]
 		last_id = messages[0].id
-	messages = messages.order_by('timestamp')
-	print(messages)
+	else:
+		messages = messages.order_by('timestamp')
 	context = {
 	'form': form,
 	'recipient':recipient,
@@ -71,9 +72,38 @@ def receiver(request):
 			    'message_id'  : message.id,
 			    }),
 			})
-			return HttpResponse("successful")
-		return HttpResponse("unsuccessful")
-	return HttpResponse("unsuccessful")
+			return HttpResponse(json.dumps({'success':True, 'html':""}))
+		return HttpResponse(json.dumps({'success':False, 'html':"Invalid Message Form"}))
+	return HttpResponse(json.dumps({'success':False, 'html':"Request type error"}))
 
-def load_more(request):
-	pass
+def load_more(request, id):
+	try:
+		recipient = User.objects.get(id=id)
+	except Exception as e:
+		print(e)
+		return HttpResponse(json.dumps({'success':False, 'html':""}))
+	try:
+		message_id = int(request.POST.get("message_id"))
+	except Exception as e:
+		print(e)
+		return HttpResponse(json.dumps({'success':False, 'html':""}))
+	mymessages = Message.objects.filter(sender=request.user, recipient=recipient, id__lte=(message_id-1))
+	tomessages = Message.objects.filter(sender=recipient, recipient=request.user, id__lte=(message_id-1))
+	messages = tomessages.union(mymessages)
+	print(messages.count())
+	last_id = -1
+	cnt = messages.count()
+	if cnt>=50:
+		messages = messages.order_by('timestamp')[cnt-49:cnt]
+		last_id = messages[0].id
+	else:
+		messages = messages.order_by('timestamp')
+	html = ""
+	sender = False
+	for message in messages:
+		if message.sender == request.user:
+			sender = True
+		else:
+			sender = False
+		html = html+render_to_string('message_channels/reply.html', {'message':message, 'sender': sender,})
+	return HttpResponse(json.dumps({'success':True, 'html':html, 'last_id':last_id}))
